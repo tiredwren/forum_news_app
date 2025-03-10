5ee791cdbd449b2c49dbe90b5b4cc4b938748af3
tiredwren <moudgalya.sneha@gmail.com>
2025-03-09 19:52:21 -0700

    ui & search fixed a bit

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
 
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'The Forum',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: ArticlesScreen(),
@@ -38,111 +39,43 @@ class _ArticlesScreenState extends State<ArticlesScreen> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 3, // Number of tabs
+      length: 3,
       child: Scaffold(
-        appBar: AppBar(
-          title: Text('The Forum'),
-        ),
+        appBar: AppBar(title: Text('The Forum')),
         body: Column(
           children: [
-            // Search Bar
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                controller: _searchController,
-                decoration: InputDecoration(
-                  hintText: 'Search articles...',
-                  prefixIcon: Icon(Icons.search),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                    borderSide: BorderSide(width: 1),
+              child: SizedBox(
+                width: double.infinity,
+                child: TextField(
+                  controller: _searchController,
+                  decoration: InputDecoration(
+                    hintText: 'Search for an article...',
+                    prefixIcon: Icon(Icons.search),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
                   ),
+                  onChanged: (value) {
+                    setState(() {
+                      _searchQuery = value.toLowerCase();
+                    });
+                  },
                 ),
-                onChanged: (value) {
-                  setState(() {
-                    _searchQuery = value.toLowerCase();
-                  });
-                },
               ),
             ),
-            // Carousel for featured articles
-            StreamBuilder<QuerySnapshot>(
-              stream: articles.where('isFeatured', isEqualTo: true).snapshots(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Center(child: CircularProgressIndicator());
-                }
-                if (snapshot.hasError) {
-                  return Center(child: Text('Error: ${snapshot.error}'));
-                }
-                if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-                  return Container(); // No featured articles
-                }
-
-                return CarouselSlider(
-                  options: CarouselOptions(
-                    height: 200,
-                    autoPlay: true,
-                    aspectRatio: 16 / 9,
-                    viewportFraction: 0.9,
-                    enlargeCenterPage: true,
-                  ),
-                  items: snapshot.data!.docs.map((doc) {
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => ArticleDetailScreen(article: doc),
-                          ),
-                        );
-                      },
-                      child: Container(
-                        margin: EdgeInsets.symmetric(horizontal: 5),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(12),
-                          image: DecorationImage(
-                            image: NetworkImage(doc['imageUrl'] ?? ''),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        child: Center(
-                          child: Text(
-                            doc['title'] ?? 'Untitled',
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                              backgroundColor: Colors.black54,
-                            ),
-                          ),
-                        ),
-                      ),
-                    );
-                  }).toList(),
-                );
-              },
-            ),
-            // TabBar for article types
-            TabBar(
-              tabs: [
-                Tab(text: 'news'),
-                Tab(text: 'public health'),
-                Tab(text: 'sports'), // Add more tabs as needed
-              ],
-            ),
-            // TabBarView for different types of articles
+            if (_searchQuery.isEmpty) _buildFeaturedArticles(),
             Expanded(
-              child: TabBarView(
+              child: _searchQuery.isEmpty
+                  ? TabBarView(
                 children: [
-                  // News Articles Tab
                   _buildArticleList(articles.where('category', isEqualTo: 'news')),
-                  // Public Health Articles Tab
                   _buildArticleList(articles.where('category', isEqualTo: 'public_health')),
-                  // Sports Articles Tab
                   _buildArticleList(articles.where('category', isEqualTo: 'sports')),
                 ],
-              ),
+              )
+                  : _buildSearchResults(),
             ),
           ],
         ),
@@ -150,7 +83,64 @@ class _ArticlesScreenState extends State<ArticlesScreen> {
     );
   }
 
-  // Function to build the list of articles for a given category
+  Widget _buildFeaturedArticles() {
+    return StreamBuilder<QuerySnapshot>(
+      stream: articles.where('isFeatured', isEqualTo: true).snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        }
+        if (snapshot.hasError || !snapshot.hasData || snapshot.data!.docs.isEmpty) {
+          return Container();
+        }
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: SizedBox(
+            width: double.infinity,
+            child: CarouselSlider(
+              options: CarouselOptions(
+                height: 200,
+                autoPlay: true,
+                enlargeCenterPage: true,
+              ),
+              items: snapshot.data!.docs.map((doc) {
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ArticleDetailScreen(article: doc),
+                      ),
+                    );
+                  },
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(12),
+                    child: Stack(
+                      fit: StackFit.expand,
+                      children: [
+                        Image.network(doc['imageUrl'] ?? '', fit: BoxFit.cover),
+                        Container(
+                          color: Colors.black54,
+                          alignment: Alignment.center,
+                          padding: EdgeInsets.all(8),
+                          child: Text(
+                            doc['title'] ?? 'Untitled',
+                            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              }).toList(),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
   Widget _buildArticleList(Query query) {
     return StreamBuilder<QuerySnapshot>(
       stream: query.orderBy('timestamp', descending: true).snapshots(),
@@ -158,49 +148,61 @@ class _ArticlesScreenState extends State<ArticlesScreen> {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return Center(child: CircularProgressIndicator());
         }
-        if (snapshot.hasError) {
-          return Center(child: Text('Error: ${snapshot.error}'));
-        }
-        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+        if (snapshot.hasError || !snapshot.hasData || snapshot.data!.docs.isEmpty) {
           return Center(child: Text('No articles found.'));
         }
+        return _buildArticleListView(snapshot.data!.docs);
+      },
+    );
+  }
 
-        // Filter articles based on the search query
+  Widget _buildSearchResults() {
+    return StreamBuilder<QuerySnapshot>(
+      stream: articles.orderBy('timestamp', descending: true).snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        }
+        if (snapshot.hasError || !snapshot.hasData || snapshot.data!.docs.isEmpty) {
+          return Center(child: Text('No articles found.'));
+        }
         final filteredDocs = snapshot.data!.docs.where((doc) {
-          final title = doc['title']?.toLowerCase() ?? '';
-          return title.contains(_searchQuery);
+          final titleMatches = doc['title']?.toLowerCase().contains(_searchQuery) ?? false;
+          final authorMatches = doc['author']?.toLowerCase().contains(_searchQuery) ?? false;
+          return titleMatches || authorMatches;
         }).toList();
 
-        return ListView(
-          padding: EdgeInsets.all(10),
-          children: filteredDocs.map((doc) {
-            return Card(
-              margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-              elevation: 5,
-              child: ListTile(
-                contentPadding: EdgeInsets.all(10),
-                leading: doc['imageUrl'] != null
-                    ? Image.network(doc['imageUrl'], width: 80, height: 80, fit: BoxFit.cover)
-                    : Icon(Icons.image, size: 80, color: Colors.grey),
-                title: Text(
-                  doc['title'] ?? 'Untitled',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-                subtitle: Text(doc['author'] ?? 'Unknown Author', style: TextStyle(fontSize: 14, fontStyle: FontStyle.italic)),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => ArticleDetailScreen(article: doc),
-                    ),
-                  );
-                },
-              ),
-            );
-          }).toList(),
-        );
+        return _buildArticleListView(filteredDocs);
       },
+    );
+  }
+
+  Widget _buildArticleListView(List<QueryDocumentSnapshot> docs) {
+    return ListView(
+      padding: EdgeInsets.all(10),
+      children: docs.map((doc) {
+        return Card(
+          margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+          elevation: 5,
+          child: ListTile(
+            contentPadding: EdgeInsets.all(10),
+            leading: doc['imageUrl'] != null
+                ? Image.network(doc['imageUrl'], width: 80, height: 80, fit: BoxFit.cover)
+                : Icon(Icons.image, size: 80, color: Colors.grey),
+            title: Text(doc['title'] ?? 'Untitled', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            subtitle: Text(doc['author'] ?? 'Unknown Author', style: TextStyle(fontSize: 14, fontStyle: FontStyle.italic)),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ArticleDetailScreen(article: doc),
+                ),
+              );
+            },
+          ),
+        );
+      }).toList(),
     );
   }
 }
@@ -218,7 +220,7 @@ class ArticleDetailScreen extends StatelessWidget {
         leading: IconButton(
           icon: Icon(Icons.arrow_back),
           onPressed: () {
-            Navigator.pop(context); // Back button functionality
+            Navigator.pop(context);
           },
         ),
       ),
@@ -248,42 +250,10 @@ class ArticleDetailScreen extends StatelessWidget {
               style: TextStyle(fontSize: 16, fontStyle: FontStyle.italic),
             ),
             SizedBox(height: 20),
-            _buildFormattedText(article['content']),
+            Text(article['content'] ?? '', style: TextStyle(fontSize: 16)),
           ],
         ),
       ),
     );
-  }
-
-  Widget _buildFormattedText(String content) {
-    List<TextSpan> spans = [];
-    List<String> paragraphs = content.split('\n\n');
-
-    for (String paragraph in paragraphs) {
-      if (paragraph.startsWith('# ')) {
-        spans.add(
-          TextSpan(
-            text: '${paragraph.substring(2)}\n\n',
-            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-          ),
-        );
-      } else if (paragraph.startsWith('## ')) {
-        spans.add(
-          TextSpan(
-            text: '${paragraph.substring(3)}\n\n',
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
-          ),
-        );
-      } else {
-        spans.add(
-          TextSpan(
-            text: '$paragraph\n\n',
-            style: TextStyle(fontSize: 16),
-          ),
-        );
-      }
-    }
-
-    return RichText(text: TextSpan(style: TextStyle(color: Colors.black), children: spans));
   }
 }
